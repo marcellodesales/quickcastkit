663c7fdd996b279eb64aa3a92b81747bbd2181fb
Andy Smart <andy@andysmart.org>
2013-08-07 19:28:41 +0100

    Initial commit

---
+++ QuickCastKit/QuickCastKit.h
@@ -0,0 +1,13 @@
+//
+//  QuickCastKit.h
+//  QuickCastKit
+//
+//  Created by Andy Smart on 07/08/2013.
+//  Copyright (c) 2013 Rocket Town Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QuickCastKit : NSObject
+
+@end
---
+++ QuickCastKit/QuickCastKit.m
@@ -0,0 +1,13 @@
+//
+//  QuickCastKit.m
+//  QuickCastKit
+//
+//  Created by Andy Smart on 07/08/2013.
+//  Copyright (c) 2013 Rocket Town Ltd. All rights reserved.
+//
+
+#import "QuickCastKit.h"
+
+@implementation QuickCastKit
+
+@end
